3b20c14afe223bae57b1bfc43969f8d55056551a
HylianRaichu <48845478+HylianRaichu@users.noreply.github.com>
2024-12-04 10:08:23 -0500

    Update move_constants.asm

--- constants/move_constants.asm
+++ constants/move_constants.asm
@@ -172,10 +172,11 @@
 	const SUBSTITUTE   ; a4
 	const POWDER_SNOW  ; a5
 	const MUD_SLAP	   ; a6
+	const CRUNCH	   ; a7
 
 NUM_ATTACKS EQU const_value - 1
 
-	const STRUGGLE     ; a7
+	const STRUGGLE     ; a8
 
 	; Moves do double duty as animation identifiers.
 
